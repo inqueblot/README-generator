49a7107fcc753dd414583f98e9d332b37f7ddc3c
inqueblot <notminebydesign@gmail.com>
2021-01-25 20:11:01 -0600

    got license question running, made const question

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ console.log(`Let's make a README! \n`)
 
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt({
+
+
+const questions = [
+    {
         //pass questions here
         name: 'Title',
         message: 'What is the name of your project?'
-    })
-    .then(answer => {
-        //user feedback for...whatever!!
-    })
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Does your project need a license?',
+        choices: ["MIT", "ICS", "that other one"],
+    },
+]
+
+
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+})
     .catch(error => {
         if (error.isTtyError) {
             "prompt couldn't be rendered in the current environment"
