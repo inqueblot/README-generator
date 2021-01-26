e7414e7cef79cad6ce9653484a8a1943c2f75248
inqueblot <notminebydesign@gmail.com>
2021-01-25 19:30:26 -0600

    ask user for readme title

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+console.log(`Let's make a README! \n`)
+
 const inquirer = require('inquirer');
 
 inquirer
     .prompt({
         //pass questions here
+        name: 'Title',
+        message: 'What is the name of your project?'
     })
     .then(answer => {
         //user feedback for...whatever!!
